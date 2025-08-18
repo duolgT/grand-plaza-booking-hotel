96e885228073d5b3c7fc2e77e443c7ecec7aeb15
DuolDaniel <duolkuach100@gmail.com>
2025-08-18 11:22:30 +0300

    Featured page

--- src/components/FeaturedDestination.jsx
+++ src/components/FeaturedDestination.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { roomsDummyData } from "../assets/assets";
 import HotelCard from "./HotelCard";
+import { useNavigate } from "react-router-dom";
 
 const FeaturedDestination = () => {
+
+  const navigate = useNavigate();
   return (
     <section className="py-12 px-6 bg-gray-50">
       <h2 className="text-3xl font-bold text-center mb-8">
         Featured Destinations
       </h2>
+      <p className="text-gray-500 mt-2 text-sm">
+    Explore our most popular stays around the world
+  </p>
 
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-4">
         {roomsDummyData.slice(0, 4).map((room, index) => (
           <HotelCard key={room._id || index} room={room} index={index} />
         ))}
       </div>
+      <button
+        onClick={() => {
+          navigate('/rooms');
+          window.scrollTo(0, 0);
+        }}
+        className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer ">
+        View All Destinations
+      </button>
     </section>
   );
 };
